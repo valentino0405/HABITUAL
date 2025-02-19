1637937136348efd378fd8fde506a451903417b8
valentino0405 <155987346+valentino0405@users.noreply.github.com>
2025-02-19 23:25:02 +0530

    Update App.js for adding chandresh ka homepage ka routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,13 @@ import SignUp from "./component/loginSignup/SignUp";
 import EntryPage from "./component/entry/EntryPage";
 import Home from "./component/homepage/Home"
 import DataProvider from './context/DataProvider';
+import UserProfile from "./component/user/UserProfile";
+import LeaderBoard from "./component/leaderboard/LeaderBoard";
+import ChatBot from "./component/chatbot/ChatBot";
+import DashBoard from "./component/dashboard/DashBoard";
+import Ecommerce from "./component/ecommerce/Ecommerce";
+import DoctorAppor from "./component/doctorappor/DoctorAppor";
+import HabitJR from "./component/habitjr/HabitJR";
 function App() {
   return (
   <DataProvider>
@@ -13,6 +20,13 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
          <Route path="/home" element={<Home/>}/>
+           <Route path="/userpro" element={<UserProfile />} />
+        <Route path="/leaderboard" element={<LeaderBoard />} />
+        <Route path="/chatbot" element={<ChatBot />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/ecommerce" element={<Ecommerce />} />
+        <Route path="/doctorappor" element={<DoctorAppor />} />
+        <Route path="/habitjr" element={<HabitJR />} />
       </Routes>
     </Router> 
     </DataProvider>
